b3d5a7b9a02908fb019225b886da9bd18ee92ffb
akintolatech <akintolatechnologies@gmail.com>
2024-09-03 23:25:35 +0100

    Bot algorithm development

---
+++ botengine/bot_algorithm.py
@@ -0,0 +1,20 @@
+
+def booking():
+    sum = 1
+
+
+    if sum == 5:
+        return True
+
+
+def login():
+    return True
+
+
+def run_bot():
+    if login():
+        if booking():
+            print("True")
+
+
+run_bot()
